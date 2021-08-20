f966a0cd4176071bd88ea00a8dfba3b11f6af822
Mowgle88 <mowgle1988@gmail.com>
2021-08-20 15:39:50 +0300

    start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsPair = {};
+	let openBrackets = [];
+	for(let i = 0; i < bracketsConfig.length; i++) {
+		bracketsPair[bracketsConfig[i][1]] = bracketsConfig[i][0];
+		openBrackets.push(bracketsConfig[i][0])
+	} 
+	// console.log(bracketsPair, openBrackets)
+	
+		let stack = [];
+
+		for(let j = 0; j < str.length; j++) {
+			let currentSymbol = str[j];
+
+			if(openBrackets.includes(currentSymbol)) {
+				stack.push(currentSymbol);
+			} else {
+				if(stack.length === 0) {
+					return false; 
+				}
+
+				let topElement = stack[stack.length - 1];
+
+				if (bracketsPair[currentSymbol] === topElement) {
+					stack.pop();
+				} else {
+					return false;
+				}
+			}
+		}
+		
+		return stack.length === 0;
 }
